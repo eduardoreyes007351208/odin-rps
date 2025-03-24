1f10b934285e74dfb7cbca01ed7fd8fa704b888e
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-03-24 15:41:20 -0700

    feat: Create function to receive computer choice
    Create function getComputerChoice which randomly
    generates a number and depending on the number,
    the computer will choose 'Rock', 'Paper', or 'Scissors'

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello")
+
+let getComputerChoice = () => {
+    let randnum = Math.floor(Math.random() * 3);
+    let choice = '';
+
+    switch(randnum) {
+        case 0:
+            choice = 'Rock';
+            break;
+        case 1:
+            choice = 'Paper';
+            break;
+        default:
+            choice = 'Scissors';
+            break;
+    }
+    
+    return choice;
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
