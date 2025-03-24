a1edc8370ca44f8622c4ff5d75eb7d9f52c5ccd0
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-03-24 16:59:03 -0700

    feat: Create function to play a round of the game
    Create playRound function which takes computerChoice
    and humanChoice and compares the choices to decide
    the winner. It also increases the score for whoever
    won.

--- script.js
+++ script.js
@@ -23,11 +23,36 @@ let getHumanChoice = () => {
     return answer;
 }
 
+let playRound = (computerChoice, humanChoice) => {
+    if (humanChoice == computerChoice) {
+        return "Tie!"
+    } else if (humanChoice == 'Rock' && computerChoice == 'Paper') {
+        computerScore++;
+        return "You lose! Paper beats Rock"
+    } else if (humanChoice == 'Rock' && computerChoice == 'Scissors') {
+        humanScore++;
+        return "You win! Rock beats Scissors"
+    } else if (humanChoice == 'Paper' && computerChoice == 'Rock') {
+        humanScore++;
+        return "You win! Rock beats Scissors"
+    }  else if (humanChoice == 'Paper' && computerChoice == 'Scissors') {
+        computerScore++;
+        return "You lose! Paper beats Rock"
+    } else if (humanChoice == 'Scissors' && computerChoice == 'Rock') {
+        computerScore++;
+        return "You lose! Paper beats Rock"
+    } else if (humanChoice == 'Scissors' && computerChoice == 'Paper') {
+        humanScore++;
+        return "You win! Rock beats Scissors"
+    }
+}
+
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
 let computerScore = 0;
 let humanScore = 0;
 
-
 console.log(computerChoice);
 console.log(humanChoice)
+console.log(playRound(computerChoice, humanChoice))
+console.log(`Computer Score: ${computerScore}, Human Score: ${humanScore}`)
